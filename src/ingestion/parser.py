16bbc31ac489524a6e5631418d1506809684bf98
anjaneyajavvadi <anjaneyajavvadi@gmail.com>
2026-04-25 22:57:51 +0530

    Data Parser Completed

---
+++ src/ingestion/parser.py
@@ -0,0 +1,25 @@
+from datasets import load_from_disk
+from src.ingestion.chunker import ContextAwareChunker
+
+class BEIRParser:
+    def __init__(self):
+        self.data_path='../data/raw/fiqa_corpus'
+        self.chunker=ContextAwareChunker()
+        self.load()
+
+    def load(self):
+        self.dataset=load_from_disk(self.data_path)
+
+    def parse(self):
+        parsed_chunks=[]
+        for doc in self.dataset['corpus']:
+            id=doc['id']
+            title=doc['title']
+            text=doc['text']
+
+            chunks=self.chunker.chunk(text)
+            for chunk in chunks:
+                chunk_dict={'id':id,'title':title,'text':text,'chunk':chunk}
+                parsed_chunks.append(chunk_dict)
+
+        return parsed_chunks
